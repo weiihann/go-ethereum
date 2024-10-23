954be8830ad5b0e441b0c95744c7d85dcba2cc2d
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2024-10-23 17:28:03 +0200

    remove ineffectual check (#518)
    Signed-off-by: Guillaume Ballet <3272758+gballet@users.noreply.github.com>

--- core/vm/gas_table.go
+++ core/vm/gas_table.go
@@ -499,12 +499,6 @@ func gasSelfdestruct(evm *EVM, contract *Contract, stack *Stack, mem *Memory, me
 		}
 	}
 
-	if evm.chainRules.IsVerkle {
-		// TODO turn this into a panic (when we are sure this method
-		// will never execute when verkle is enabled)
-		log.Warn("verkle witness accumulation not supported for selfdestruct")
-	}
-
 	if !evm.StateDB.HasSelfDestructed(contract.Address()) {
 		evm.StateDB.AddRefund(params.SelfdestructRefundGas)
 	}
